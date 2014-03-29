d47a4023d68ab2b4c2091c6cac9c861186d024fc
Cengizism <culusoy@ask-cs.com>
2014-03-29 06:42:17 +0100

    some updates

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,7 +16,7 @@ module.exports = function (grunt)
 
   var appConfig = {
     app:  'app',
-    dist: 'war'
+    dist: 'dist'
   };
 
   try {
@@ -78,7 +78,7 @@ module.exports = function (grunt)
         options: {
           port: 9000,
           // Change this to '0.0.0.0' to access the server from outside.
-          hostname: 'localhost'
+          hostname: '0.0.0.0' //'localhost'
         },
         livereload: {
           options: {
